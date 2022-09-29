43fa8a73412c012c4d73df719de410e81bf6d873
Masterbond7 <jamesslough4@gmail.com>
2022-09-29 15:18:29 +1300

    Added code to set libusb log level

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,24 @@
 #include <libusb-1.0/libusb.h>
 
 int result = 0;
-char exit_code = 0;
 
 int main() {
     // Initialize libusb
     result = libusb_init(NULL);
     if (result != 0) {
-        std::cout << "Error initializing LIBUSB: " << libusb_error_name(result) << std::endl;
-        exit_code = 1; // ERROR: Failed to initialize libusb
+        std::cout << "Error initializing libusb: " << libusb_error_name(result) << std::endl;
+        return 1; // ERROR: Failed to initialize libusb
+    }
+
+    result = libusb_set_option(NULL, LIBUSB_OPTION_LOG_LEVEL, LIBUSB_LOG_LEVEL_WARNING);
+    if (result != 0) {
+        std::cout << "Error setting libusb log level: " << libusb_error_name(result) << std::endl;
+        return 1;
     }
 
     // Deinitialize libusb
     libusb_exit(NULL);
 
     // Exit successfully
-    return exit_code;
+    return 0;
 }
