e01608482beb5db4e5148c91e3abe1f0dad9aef1
Masterbond7 <jamesslough4@gmail.com>
2022-09-29 17:12:21 +1300

    Added libusb_context variable so that the default context is not used

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 #include <libusb-1.0/libusb.h>
 
+// Core program variables
 int result = 0;
+libusb_context *usb_context;
 
 int main() {
     // Initialize libusb
-    result = libusb_init(NULL);
+    result = libusb_init(&usb_context);
     if (result != 0) {
         std::cout << "Error initializing libusb: " << libusb_error_name(result) << std::endl;
         return 1; // ERROR: Failed to initialize libusb
     }
 
-    result = libusb_set_option(NULL, LIBUSB_OPTION_LOG_LEVEL, LIBUSB_LOG_LEVEL_WARNING);
+    result = libusb_set_option(usb_context, LIBUSB_OPTION_LOG_LEVEL, LIBUSB_LOG_LEVEL_WARNING);
     if (result != 0) {
         std::cout << "Error setting libusb log level: " << libusb_error_name(result) << std::endl;
-        return 1;
+        return 1; // ERROR: Failed to initialize libusb
     }
 
     // Deinitialize libusb
-    libusb_exit(NULL);
+    libusb_exit(usb_context);
 
     // Exit successfully
     return 0;
